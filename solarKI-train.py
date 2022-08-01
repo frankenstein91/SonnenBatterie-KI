86cba1bceeafe8d373d6c6c3c79f257ddf65a6e7
Frank Tornack <frankenstein91.info@googlemail.com>
2022-08-01 23:42:30 +0200

    code reformat

--- solarKI-train.py
+++ solarKI-train.py
@@ -2,45 +2,60 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import tensorflow as tf
-import pandas as pd
 import os
-# import keras
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, LSTM
-from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.python.keras.layers import LSTM, Dense
+from tensorflow.python.keras.models import Sequential
 
 dataBoundaries = {
-    "measurements": {
-        "battery_state_of_charge": {
-            "min": 0.0,
-            "max": 100.0
-            }
-        },
-    "statistics": {
-        }
-    }
+    "measurements": {"battery_state_of_charge": {"min": 0.0, "max": 100.0}},
+    "statistics": {},
+}
+
 
 def main():
     # create parser
-    parser = argparse.ArgumentParser(description='SolarKI')
+    parser = argparse.ArgumentParser(description="SolarKI")
     # add arguments
     # file path to measurements & statistics CSV
-    parser.add_argument('--measurements', type=str, default='measurements.csv', help='file path to measurements CSV')
-    parser.add_argument('--statistics', type=str, default='statistics.csv', help='file path to statistics CSV')
+    parser.add_argument(
+        "--measurements",
+        type=str,
+        default="measurements.csv",
+        help="file path to measurements CSV",
+    )
+    parser.add_argument(
+        "--statistics",
+        type=str,
+        default="statistics.csv",
+        help="file path to statistics CSV",
+    )
     # add config group
-    ConfigArgs = parser.add_argument_group('Config', 'Configurations')
+    ConfigArgs = parser.add_argument_group("Config", "Configurations")
     # CSV delimiter
-    ConfigArgs.add_argument('--delimiter', type=str, default=',', help='CSV delimiter')
+    ConfigArgs.add_argument("--delimiter", type=str, default=",", help="CSV delimiter")
     # path to checkpoint directory
-    ConfigArgs.add_argument('--checkpoint_dir', type=str, default='checkpoint', help='path to checkpoint directory')
+    ConfigArgs.add_argument(
+        "--checkpoint_dir",
+        type=str,
+        default="checkpoint",
+        help="path to checkpoint directory",
+    )
     # path to the log directory
-    ConfigArgs.add_argument('--log_dir', type=str, default='log', help='path to the log directory')
+    ConfigArgs.add_argument(
+        "--log_dir", type=str, default="log", help="path to the log directory"
+    )
     # path to the model directory
-    ConfigArgs.add_argument('--model_dir', type=str, default='model', help='path to the model directory')
+    ConfigArgs.add_argument(
+        "--model_dir", type=str, default="model", help="path to the model directory"
+    )
     # splitrate of training data
-    ConfigArgs.add_argument('--splitrate', type=float, default=0.8, help='splitrate of training data')
+    ConfigArgs.add_argument(
+        "--splitrate", type=float, default=0.8, help="splitrate of training data"
+    )
     # parse arguments to local variables
     args = parser.parse_args()
     # create checkpoint directory if it doesn't exist
@@ -49,21 +64,31 @@ def main():
     # create log directory if it doesn't exist
     if not os.path.exists(args.log_dir):
         os.makedirs(args.log_dir)
-    #create sub dirs if not exist
-    if not os.path.exists(os.path.join(args.log_dir,"measurements_model")):
-        os.makedirs(os.path.join(args.log_dir,"measurements_model"))
-    if not os.path.exists(os.path.join(args.log_dir,"statistics_model")):
-        os.makedirs(os.path.join(args.log_dir,"statistics_model"))
+    # create sub dirs if not exist
+    if not os.path.exists(os.path.join(args.log_dir, "measurements_model")):
+        os.makedirs(os.path.join(args.log_dir, "measurements_model"))
+    if not os.path.exists(os.path.join(args.log_dir, "statistics_model")):
+        os.makedirs(os.path.join(args.log_dir, "statistics_model"))
     # create model directory if it doesn't exist
     if not os.path.exists(args.model_dir):
         os.makedirs(args.model_dir)
     # read both datasets
-    measurements = pd.read_csv(args.measurements, delimiter=args.delimiter,parse_dates=["timestamp"], index_col="timestamp")
-    statistics = pd.read_csv(args.statistics, delimiter=args.delimiter,parse_dates=["timestamp"], index_col="timestamp")
+    measurements = pd.read_csv(
+        args.measurements,
+        delimiter=args.delimiter,
+        parse_dates=["timestamp"],
+        index_col="timestamp",
+    )
+    statistics = pd.read_csv(
+        args.statistics,
+        delimiter=args.delimiter,
+        parse_dates=["timestamp"],
+        index_col="timestamp",
+    )
     # sort datasets by timestamp
-    measurements.sort_values(by=['timestamp'], inplace=True, ascending=True)
-    statistics.sort_values(by=['timestamp'], inplace=True, ascending=True)
-        
+    measurements.sort_values(by=["timestamp"], inplace=True, ascending=True)
+    statistics.sort_values(by=["timestamp"], inplace=True, ascending=True)
+
     # print overview of datasets
     print("Measurements:")
     print(measurements.describe().transpose())
@@ -75,10 +100,10 @@ def main():
     print("Statistics:")
     print(statistics.info())
     # split datasets into train and test
-    train_measurements = measurements[:int(len(measurements)*args.splitrate)]
-    test_measurements = measurements[int(len(measurements)*args.splitrate):]
-    train_statistics = statistics[:int(len(statistics)*args.splitrate)]
-    test_statistics = statistics[int(len(statistics)*args.splitrate):]
+    train_measurements = measurements[: int(len(measurements) * args.splitrate)]
+    test_measurements = measurements[int(len(measurements) * args.splitrate) :]
+    train_statistics = statistics[: int(len(statistics) * args.splitrate)]
+    test_statistics = statistics[int(len(statistics) * args.splitrate) :]
     # print info
     print("Train measurements:")
     print(train_measurements.info())
@@ -98,11 +123,19 @@ def main():
     print("Test statistics:")
     print(test_statistics.head())
     # convert datasets to tensors
-    train_measurements_tensor = tf.convert_to_tensor(train_measurements.to_numpy().reshape(-1, 1, len(train_measurements.columns)))
-    test_measurements_tensor = tf.convert_to_tensor(test_measurements.to_numpy().reshape(-1, 1, len(test_measurements.columns)))
-    train_statistics_tensor = tf.convert_to_tensor(train_statistics.to_numpy().reshape(-1, 1, len(train_statistics.columns)))
-    test_statistics_tensor = tf.convert_to_tensor(test_statistics.to_numpy().reshape(-1, 1, len(test_statistics.columns)))
-    
+    train_measurements_tensor = tf.convert_to_tensor(
+        train_measurements.to_numpy().reshape(-1, 1, len(train_measurements.columns))
+    )
+    test_measurements_tensor = tf.convert_to_tensor(
+        test_measurements.to_numpy().reshape(-1, 1, len(test_measurements.columns))
+    )
+    train_statistics_tensor = tf.convert_to_tensor(
+        train_statistics.to_numpy().reshape(-1, 1, len(train_statistics.columns))
+    )
+    test_statistics_tensor = tf.convert_to_tensor(
+        test_statistics.to_numpy().reshape(-1, 1, len(test_statistics.columns))
+    )
+
     # create LSTM model for time series prediction of measurements
     # the model should be able to predict the next 48 hours of measurements
     # list of measurements: production, consumption, battery_charge, battery_discharge, grid_feedin, grid_consumption, battery_state_of_charge, direct_consumption
@@ -114,7 +147,7 @@ def main():
     # add output layer
     measurements_model.add(Dense(8))
     # compile model
-    measurements_model.compile(loss='mean_squared_error', optimizer='adam')
+    measurements_model.compile(loss="mean_squared_error", optimizer="adam")
     # print model summary
     print(measurements_model.summary())
     # create LSTM model for time series prediction of statistics
@@ -128,32 +161,78 @@ def main():
     # add output layer
     statistics_model.add(Dense(6))
     # compile model
-    statistics_model.compile(loss='mean_squared_error', optimizer='adam')
+    statistics_model.compile(loss="mean_squared_error", optimizer="adam")
     # print model summary
     print(statistics_model.summary())
     # create checkpoint callback
-    measurements_model_checkpoint_callback = ModelCheckpoint(filepath=os.path.join(args.checkpoint_dir, "measurements_model_checkpoint.h5"), save_weights_only=False, monitor='val_loss', mode='min', save_best_only=True)
-    statistics_model_checkpoint_callback = ModelCheckpoint(filepath=os.path.join(args.checkpoint_dir, "statistics_model_checkpoint.h5"), save_weights_only=False, monitor='val_loss', mode='min', save_best_only=True)
+    measurements_model_checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(args.checkpoint_dir, "measurements_model_checkpoint.h5"),
+        save_weights_only=False,
+        monitor="val_loss",
+        mode="min",
+        save_best_only=True,
+    )
+    statistics_model_checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(args.checkpoint_dir, "statistics_model_checkpoint.h5"),
+        save_weights_only=False,
+        monitor="val_loss",
+        mode="min",
+        save_best_only=True,
+    )
     # create early stopping callback
-    measurements_model_earlystopping_callback = EarlyStopping(monitor='val_loss', mode='min', patience=10)
-    statistics_model_earlystopping_callback = EarlyStopping(monitor='val_loss', mode='min', patience=10)
+    measurements_model_earlystopping_callback = EarlyStopping(
+        monitor="val_loss", mode="min", patience=10
+    )
+    statistics_model_earlystopping_callback = EarlyStopping(
+        monitor="val_loss", mode="min", patience=10
+    )
     # create tensorboard callback
-    measurements_model_tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(args.log_dir,"measurements_model"), histogram_freq=1)
-    statistics_model_tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(args.log_dir,"statistics_model"), histogram_freq=1)
+    measurements_model_tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(args.log_dir, "measurements_model"), histogram_freq=1
+    )
+    statistics_model_tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(args.log_dir, "statistics_model"), histogram_freq=1
+    )
 
     # train models
-    measurements_model.fit(train_measurements_tensor, train_measurements_tensor, epochs=150, batch_size=32, validation_data=(test_measurements_tensor, test_measurements_tensor), callbacks=[measurements_model_checkpoint_callback, measurements_model_earlystopping_callback, measurements_model_tensorboard_callback])
-    statistics_model.fit(train_statistics_tensor, train_statistics_tensor, epochs=150, batch_size=32, validation_data=(test_statistics_tensor, test_statistics_tensor), callbacks=[statistics_model_checkpoint_callback, statistics_model_earlystopping_callback, statistics_model_tensorboard_callback])
+    measurements_model.fit(
+        train_measurements_tensor,
+        train_measurements_tensor,
+        epochs=150,
+        batch_size=32,
+        validation_data=(test_measurements_tensor, test_measurements_tensor),
+        callbacks=[
+            measurements_model_checkpoint_callback,
+            measurements_model_earlystopping_callback,
+            measurements_model_tensorboard_callback,
+        ],
+    )
+    statistics_model.fit(
+        train_statistics_tensor,
+        train_statistics_tensor,
+        epochs=150,
+        batch_size=32,
+        validation_data=(test_statistics_tensor, test_statistics_tensor),
+        callbacks=[
+            statistics_model_checkpoint_callback,
+            statistics_model_earlystopping_callback,
+            statistics_model_tensorboard_callback,
+        ],
+    )
 
     # evaluate models
-    measurements_model_loss = measurements_model.evaluate(test_measurements_tensor, test_measurements_tensor, verbose=0)
-    statistics_model_loss = statistics_model.evaluate(test_statistics_tensor, test_statistics_tensor, verbose=0)
+    measurements_model_loss = measurements_model.evaluate(
+        test_measurements_tensor, test_measurements_tensor, verbose=0
+    )
+    statistics_model_loss = statistics_model.evaluate(
+        test_statistics_tensor, test_statistics_tensor, verbose=0
+    )
     print(f"Measurements model loss: {measurements_model_loss}")
     print(f"Statistics model loss: {statistics_model_loss}")
     # save models
     measurements_model.save(os.path.join(args.model_dir, "measurements_model.h5"))
     statistics_model.save(os.path.join(args.model_dir, "statistics_model.h5"))
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
